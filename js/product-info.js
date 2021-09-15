623efbadcc3601f190465c1263fc2da98fcc3ad2
matiassilva5 <matias.silva9@hotmail.com>
2021-09-15 12:54:13 -0300

    funciones ordenadas
     Falta ordenar la de agregarComentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,10 +1,10 @@
 var product = {};
 
-function showImagesGallery(array){
+function showImagesGallery(array) {
 
     let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let imageSrc = array[i];
 
         htmlContentToAppend += `
@@ -12,8 +12,7 @@ function showImagesGallery(array){
             <div class="d-block mb-4 h-100">
                 <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
             </div>
-        </div>
-        `
+        </div>`
 
         document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
     }
@@ -22,76 +21,142 @@ function showImagesGallery(array){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
+document.addEventListener("DOMContentLoaded", function (e) {
+    //INFORMACIÓN DEL PRODUCTO
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             product = resultObj.data;
 
-            let productNameHTML  = document.getElementById("productName");
+            let productNameHTML = document.getElementById("productName");
             let productDescriptionHTML = document.getElementById("productDescription");
             let productCostHTML = document.getElementById("productCost");
             let soldCountHTML = document.getElementById("soldCount");
             let categoryHTML = document.getElementById("category");
             //let relatedProductsHTML = document.getElementById("relatedProducts");
 
-        
+
             productNameHTML.innerHTML = product.name;
             productDescriptionHTML.innerHTML = product.description;
             productCostHTML.innerHTML = product.cost + ` ` + product.currency;
             soldCountHTML.innerHTML = product.soldCount;
             categoryHTML.innerHTML = product.category;
 
-/*    "relatedProducts": [1, 3]
-*/
+            /*    "relatedProducts": [1, 3]
+            */
 
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
         }
     });
 
-
-
-
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj2){
-        if (resultObj2.status === "ok")
-        {
+    //LISTA DE COMENTARIOS
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj2) {
+        if (resultObj2.status === "ok") {
             comentarios = resultObj2.data;
             insertarComentarios(comentarios);
         }
     });
 });
 
-/*En esta función vamos a agregar los comentarios al elemento html creado para contenerlos*/
-function insertarComentarios(comentarios){
+/*En esta función vamos a agregar todos los comentarios al elemento html creado para contenerlos*/
+function insertarComentarios(comentarios) {
 
     let htmlContentToAppend = "";
-    let coment; 
-    for(let i = 0; i < comentarios.length; i++){
+    let coment;
+    //en este for agrego los comentarios sacados del json
+    for (let i = 0; i < comentarios.length; i++) {
         coment = comentarios[i];
         agregarComentario(coment.user, coment.description, coment.score, coment.dateTime)
     }
-    if (localStorage.getItem("comentarios") != null){
-        var comentariosNuevos = JSON.parse(localStorage.getItem("comentarios"));
-        for(let i = 0; i < comentariosNuevos.length; i++){
+
+    if (localStorage.getItem("comentarios") != null) {//si existen comentarios nuevos (que no son del json)
+        var comentariosNuevos = JSON.parse(localStorage.getItem("comentarios"));//los convierto en un elemento js
+        //en este for agrego los comentarios nuevos que no son del json
+        for (let i = 0; i < comentariosNuevos.length; i++) {
             coment = comentariosNuevos[i];
             agregarComentario(coment.user, coment.description, coment.score, coment.dateTime)
         }
     }
 }
 
-/*Esta función sirve para crear un nuevo comentario*/
-function comentar(){
+/*Esta función agrega un comentario a la lista del html*/
+function agregarComentario(user, description, score, dateTime) {
+    var elem1 = "";
+    var elem2 = "";
+    var elem3 = "";
+    var elem4 = "";
+    var htmlContentToAppend = "";
+    elem1 = `<li class="">
+         <div class="row">
+            <div class=" comment-box" >
+
+           
+     <!-- Contenedor del Comentario -->
+        <div class="comment-head">
+            <div class="row">
+                <div class="col-md-6">
+                    <h6 class="comment-name by-author">
+                        <a href="http://creaticode.com/blog">${user}</a>
+                    </h6>
+                    <span id="time"> ${dateTime}</span>
+                </div>
+                <div class="col-md-6">
+                `
+
+    for (let i = 1; i <= 5; i++) {
+        if (i <= score) {
+            elem2 += `<span class="fa fa-star checked"></span>`
+        } else {
+            elem3 += `<span class="fa fa-star"></span>`
+        }
+    }
+    elem4 = `</div>
+           </div>
+          </div>
+            <div class="comment-content">
+               ${description}
+            </div>
+             </div>
+     </div>
+    </li>`
+
+    htmlContentToAppend = elem1 + elem2 + elem3 + elem4;
+
+    document.getElementById("lista-comentarios").innerHTML += htmlContentToAppend;
+}
+
+/*Esta función se activa cuando se quiere crear un nuevo comentario*/
+function comentar() {
     var coment = document.getElementById("newComent").value;
     var box = document.getElementById("lista-comentarios");
     var user = localStorage.getItem("usuario");
+    var score = obtenerScore();
+    var dateTime = obtenerFechaActual();
+
+    //se agrega el comentario a la lista en el html
+    agregarComentario(user, coment, score, dateTime);
+
+
+    //se guarda el comentario nuevo en el local storage
+    var nuevo = [{
+        "score": score,
+        "description": coment,
+        "user": user,
+        "dateTime": dateTime
+    }]
+    guardarComentario(nuevo);
+    document.getElementById("newComent").value = ""; /*vacío el textarea*/
+    return false;
+}
+
+function obtenerScore() {
     var estrella1 = document.getElementById("radio5");
     var estrella2 = document.getElementById("radio4");
     var estrella3 = document.getElementById("radio3");
     var estrella4 = document.getElementById("radio2");
     var estrella5 = document.getElementById("radio1");
     var score = 0
-    if(estrella5.checked)
+    if (estrella5.checked)
         score = 5
     else if (estrella4.checked)
         score = 4
@@ -102,99 +167,34 @@ function comentar(){
     else if (estrella1.checked)
         score = 1
 
+    return score;
+}
+
+function obtenerFechaActual() {
     let date = new Date();
+    let day = date.getDate().toString().padStart(2, '0');
+    let month = (date.getMonth() + 1).toString().padStart(2, '0');
+    let year = date.getFullYear().toString().padStart(2, '0');
+    var hour = date.getHours().toString().padStart(2, '0');
+    var minutes = date.getMinutes().toString().padStart(2, '0');
+    var seconds = date.getSeconds().toString().padStart(2, '0');
+    //se le da el formato deseado
+    var dateTime = `${year} -${month} -${day} ${hour}: ${minutes}: ${seconds} `;
 
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
+    return dateTime;
+}
+/* 
+"persistencia" para comentarios nuevos. 
+Existen hasta que se cierra sesión y se limpia el localstorage
+*/
+function guardarComentario(nuevo) {
+    var array = [];//inicializo el arreglo
 
-    if(month < 10){
-        month = `0${month}`;
-    }
-    if (day < 10){
-        day = `0${day}`;
-    }
-    var hour = date.getHours();
-    if (hour < 10){
-        hour = `0${hour}`;
-    }
-    var minutes = date.getMinutes();
-    if (minutes < 10){
-        minutes = `0${minutes}`;
-    }
-    var seconds = date.getSeconds();
-    if (seconds < 10){
-        seconds = `0${seconds}`;
-    }
-    var dateTime = `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`; 
+    if (localStorage.getItem("comentarios") != null) //si ya existen comentarios
+        array = JSON.parse(localStorage.getItem("comentarios"));//paso de json a objeto js
 
-    agregarComentario(user, coment, score, dateTime);
-
-
-    /* 
-    "persistencia" para comentarios nuevos. 
-    Existen hasta que se cierra sesión y se limpia el localstorage
-    */
-    var nuevo = [{"score": score,
-                    "description": coment,
-                    "user": user,
-                    "dateTime": dateTime}]
-    if (localStorage.getItem("comentarios")!=null)
-        var array = JSON.parse(localStorage.getItem("comentarios"));//paso de json a objeto js
-    else
-        var array = [];//inicializo el arreglo ya que es mi primer comentario
     array = array.concat(nuevo);
     localStorage.setItem("comentarios", JSON.stringify(array))//inserto el comentario en formato json
-
-    document.getElementById("newComent").value=""; /*vacío el textarea*/
-
-    return false;
 }
 
-/*Esta función agrega un comentario a la lista*/
-function agregarComentario(user, description, score, dateTime){
-        var elem1 = "";
-        var elem2 = "";
-        var elem3 = "";
-        var elem4 = "";
-        var htmlContentToAppend = "";
-        elem1 = `<li class="">
-             <div class="row">
-                <div class=" comment-box" >
 
-               
-         <!-- Contenedor del Comentario -->
-            <div class="comment-head">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h6 class="comment-name by-author">
-                            <a href="http://creaticode.com/blog">${user}</a>
-                        </h6>
-                        <span id="time"> ${dateTime}</span>
-                    </div>
-                    <div class="col-md-6">
-                    `
-
-        for (let i=1; i<=5;i++){
-            if (i<=score){
-                elem2 += `<span class="fa fa-star checked"></span>`
-            } else {
-                elem3 += `<span class="fa fa-star"></span>`
-            }
-        }
-        elem4 = `</div>
-               </div>
-              </div>
-                <div class="comment-content">
-                   ${description}
-                </div>
-                 </div>
-         </div>
-        </li>`
-
-    htmlContentToAppend = elem1+elem2+elem3+elem4;
-
-    document.getElementById("lista-comentarios").innerHTML += htmlContentToAppend;
-
-   
-}
